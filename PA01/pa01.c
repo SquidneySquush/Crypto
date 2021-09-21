c7e1e2e0bf3464da6745537c70a8892abdfa1434
SquidneySquush <sydneyhnat@gmail.com>
2021-09-21 14:21:57 -0400

    updates- final

--- PA01/pa01.c
+++ PA01/pa01.c
@@ -22,6 +22,7 @@ int main(int argc, const char* argv[]){
     fgets(buf, 5000, (FILE*)fp); 
     gmp_sscanf(buf, "%Zd,%Zd,%Zd,%Zd,%Zd", m_original, c2, d2, p2, q2); // read input into variables
     fclose(fp); // close file and release memory
+    //printf("input accessed\n");
 
     // generate random p/q s.t n > 1000 bits
     gmp_randstate_t state;
@@ -33,27 +34,46 @@ int main(int argc, const char* argv[]){
     mpz_init(n);
     mpz_urandomb(rand, state, 1000); // maybe change int value to be less (2^n -1)
 
+    mpz_nextprime(p, rand);
+    mpz_nextprime(q, p);
+
+    mpz_mul(n, p, q);
+    gmp_randclear(state);
+
     // while bit value of n< 1000 keep generating the next prime numbers of p and q
     while( mpz_sizeinbase(n , 2) < 1000){
         mpz_nextprime(p, q);
         mpz_nextprime(q, p);
         mpz_mul(n, p, q);
     } 
-
+    //printf("prime numbers found\n");
     // key generation
     // Compute Eulers totient function
-    mpz_t p_sub, q_sub, phi, c, d, one;
-    //mpz_t one; // has to be initiali
+    mpz_t p_sub, q_sub, phi, c, d;//, e, gcd;
+    mpz_t one; // has to be initiali
+    mpz_inits(p_sub, q_sub, phi, c, d, NULL);
     mpz_init_set_str(one, "1", 10);
     mpz_sub(p_sub, p, one);
     mpz_sub(q_sub, q, one);
+    
     mpz_mul(phi, p_sub, q_sub);
-
+    
+    //printf("euler totient complete\n");
     mpz_t e, gcd;
+    mpz_init(e);
+    mpz_init(gcd);
     gmp_randstate_t state2;
     gmp_randinit_mt(state2);
     mpz_urandomm(e, state2, phi); // randomly choose e
-
+    
+    // get gcd of phi and e and then change e until gcd =1
+    mpz_gcd(gcd, e, phi); 
+    
+    while( mpz_get_ui(gcd) != 1){
+        mpz_add(e, e, one);
+        mpz_gcd(gcd, e, phi);
+    }
+    
     // find e's multiplicative inverse mod phi and set = d
     mpz_invert(d, e, phi); 
 
@@ -61,9 +81,10 @@ int main(int argc, const char* argv[]){
     mpz_powm(c, m_original, e, n);
 
     // Decryption 
-    mpz_t m2;
-    mpz_powm(m2, c, d, n);
-
+    mpz_t m2, n2;
+    mpz_inits(m2, n2, NULL);
+    mpz_mul(n2, p2, q2);
+    mpz_powm(m2, c2, d2, n2);
 
     fp = fopen("./output", "w+");
     if (fp == NULL){
@@ -71,8 +92,8 @@ int main(int argc, const char* argv[]){
         return 0;
     }
     gmp_fprintf(fp, "%Zd,%Zd,%Zd,%Zd,%Zd", c, e, d, n, m2);
-
+    
     fclose(fp);
-
+    
     return 0;
 }
