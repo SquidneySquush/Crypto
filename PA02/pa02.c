80a826463ba2149ae994e777bd3baf28fcc6d689
SquidneySquush <sydneyhnat@gmail.com>
2021-10-08 13:29:18 -0400

    Update files

--- PA02/pa02.c
+++ PA02/pa02.c
@@ -8,8 +8,8 @@
 int main(int argc, const char* argv[]){
 
     //initialize int variables for input
-    mpz_t m, x, p, c11, c12, c21, c22, c31, c32;
-    mpz_inits(m, x, p, c11, c12, c21, c22, c31, c32, NULL);
+    mpz_t m, x, g, p, c11, c12, c21, c22, c31, c32;
+    mpz_inits(m, g, x, p, c11, c12, c21, c22, c31, c32, NULL);
     // Assign x to private key given in directions
     //mpz_init2(x, 200);
     //mpz_set_ui(x, 1234567890123456789012345678901234567890);
@@ -29,6 +29,27 @@ int main(int argc, const char* argv[]){
     fgets(buf, 5000, (FILE*)fp); 
     gmp_sscanf(buf, "%Zd", m); // read input into variables
     fclose(fp); // close file and release memory
+
+    // generate p s.t p > 2000 bits
+    gmp_randstate_t state;
+    gmp_randinit_mt(state);
+    mpz_init(rand);
+    mpz_urandomb(rand, state, 2000);
+    mpz_nextprime(p, rand);
+    mpz_urandomb(g,state,2000);
+    
+    // Make sure that p > g
+    while(mpz_cmp(p,g)<0){
+        mpz_randomb(g,state,2000);
+    }
+    
+    gmp_randclear(state);
+
+
+
+
+
+
 
     fp = fopen("./output", "w+");
     if (fp == NULL){
