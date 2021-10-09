88bb6cb5142316bbcda5d61c4a5fce29231437e7
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-10-08 22:52:01 -0400

    Update pa02.c

--- PA02/pa02.c
+++ PA02/pa02.c
@@ -4,112 +4,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <time.h>
 
 int main(int argc, const char* argv[]){
 
-    //initialize int variables for input
-    mpz_t m, x, g, p, h, r, hr, p_qr, g2, gx, c11, c12;	//, c21, c22, c31, c32;
-    mpz_inits(m, g, x, p, h, r, hr, p_qr, g2, g_x, c11, c12, NULL);		//, c21, c22, c31, c32, NULL);
-    // Assign x to private key given in directions
-    //mpz_init2(x, 200);
-    //mpz_set_ui(x, 1234567890123456789012345678901234567890);
-    const char str[] = "1234567890123456789012345678901234567890";
-    mpz_set_str(x, str, 10);    // parse the string as a base-10 number
+	//initialize int variables for input
+	mpz_t m, x, g, p, h, r, hr, p_qr, g2, gx, rand, gen, c11, c12;	//, c21, c22, c31, c32;
+	mpz_inits(m, g, x, p, h, r, hr, p_qr, g2, gx, gen, c11, c12, NULL);		//, c21, c22, c31, c32, NULL);
+	// Assign x to private key given in directions
+	//mpz_init2(x, 200);
+	//mpz_set_ui(x, 1234567890123456789012345678901234567890);
+	const char str[] = "1234567890123456789012345678901234567890";
+	mpz_set_str(x, str, 10);    // parse the string as a base-10 number
 
-    FILE *fp;
-    char buf[5000];
-    fp = fopen("./input", "r");
+	FILE *fp;
+	char buf[5000];
+	fp = fopen("./input", "r");
 
-    // check if file is empty
-    if (fp == NULL){
-        printf("\"./input\" does not exist\n");
-        return 0;
-    }
+	// check if file is empty
+	if (fp == NULL){
+		printf("\"./input\" does not exist\n");
+		return 0;
+	}
 
-    fgets(buf, 5000, (FILE*)fp); 
-    gmp_sscanf(buf, "%Zd", m); // read input into variables
-    fclose(fp); // close file and release memory
+	fgets(buf, 5000, (FILE*)fp); 
+	gmp_sscanf(buf, "%Zd", m); // read input into variables
+	fclose(fp); // close file and release memory
 
-    // generate p s.t p > 2000 bits
-    gmp_randstate_t state;
-    gmp_randinit_mt(state);
-   
-    
-    //find random g
+	// generate p s.t p > 2000 bits
+	gmp_randstate_t state;
+	gmp_randinit_mt(state);
+
+
+	//find random g
 	srand(time(0));
 	int seed = rand();
-    mpz_init(rand);
-    mpz_urandomb(rand, state, 2000);
-    mpz_nextprime(p, rand);
-    gmp_randseed_ui(state, seed);
-    mpz_urandomb(g,state,2000); //find random g
-    //mpz_urandomb(r,state,2000);
-   
+	mpz_init(rand);
+	mpz_urandomb(rand, state, 2000);
+	mpz_nextprime(p, rand);
+	gmp_randseed_ui(state, seed);
+	mpz_urandomb(g,state,2000); //find random g
+	//mpz_urandomb(r,state,2000);
 
-    mpz_set(p_qr, p);
-    mpz_submul_ui(p_qr, 1, 0.5);
+
+	mpz_set(p_qr, p);
+	mpz_set_ui(one, 1);
+	mpz_submul_ui(p_qr, 1, 0.5);
 
     //calculate generator that is safe from QR/QNR attacks
-    while (mpz_cmp(g, p) != 0) {
-        mpz_powm(g_x, g, p_qr, p); //g^((p-1)/2) mod p
-        mpz_powm_ui(g2, g, 2, p); //g^2 mod p
+	while (mpz_cmp(g, p) != 0) {
+		mpz_powm(gx, g, p_qr, p); //g^((p-1)/2) mod p
+		mpz_powm_ui(g2, g, 2, p); //g^2 mod p
 
-        if (!(mpz_cmp_ui(g_x, 1) == 0) && !(mpz_cmp_ui(g_pow_2, 1) == 0)) {
-            mpz_set(gen, g);
-            break;
-        }
-        else {
-            srand(time(0));
-            int seed = rand();
-            gmp_randseed_ui(g_state, seed);
-            mpz_urandomb(g, g_state, 2000);
-        }
+		if (!(mpz_cmp_ui(gx, 1) == 0) && !(mpz_cmp_ui(g2, 1) == 0)) {
+		    mpz_set(gen, g);
+		    break;
+		}
+		else {
+		    mpz_urandomb(g, state, 2000);
+		}
 
-    }
+	}
 
-/*
-    //make sure g is greaater than x
-    while( mpz_cmp(g,x)<0 ){ 
-        mpz_urandomb(9,state,n);
-    }
-    
-    // Make sure that p > g
-    while(mpz_cmp(p,g)<0){
-        mpz_urandomb(p,state,2000);
-        mpz_nextprime(p, p);
-    }
-   */     
-    mpz_powm(h,gen,x,p);
+	/*
+	//make sure g is greaater than x
+	while( mpz_cmp(g,x)<0 ){ 
+	mpz_urandomb(9,state,n);
+	}
 
-//------------------------Encryption-------------------------//
+	// Make sure that p > g
+	while(mpz_cmp(p,g)<0){
+	mpz_urandomb(p,state,2000);
+	mpz_nextprime(p, p);
+	}
+	*/     
+	mpz_powm(h,gen,x,p);
 
-mpz_urandomb(r,state,n);
+	//------------------------Encryption-------------------------//
 
-// make sure that p is greater than k
-    while( mpz_cmp(p,r)<0 ) {
-     mpz_urandomb(r,state,2000);
-    }
+	mpz_urandomb(r,state,2000);
 
-    mpz_powm(c11, gen, r, p);  // calculate c11
-    mpz_powm(hr, h, r, p);  // calculate h^r
-    mpz_mul_ui(c12, m, hr); // calculate c12 
+	// make sure that p is greater than k
+	while( mpz_cmp(p,r)<0 ) {
+		mpz_urandomb(r,state,2000);
+	}
 
-    //------------------------Check If QR/QNR safe-------------------------//
+	mpz_powm(c11, gen, r, p);  // calculate c11
+	mpz_powm(hr, h, r, p);  // calculate h^r
+	mpz_mul_ui(c12, m, hr); // calculate c12 
+
+	//------------------------Check If QR/QNR safe-------------------------//
 
 
 
-    gmp_randclear(state);
+	gmp_randclear(state);
 
 
-    fp = fopen("./output", "w+");
-    if (fp == NULL){
-        printf("\"./output\" does not exist.\n");
-        return 0;
-    }
-    gmp_fprintf(fp, "%Zd,%Zd,%Zd\n%Zd”,c11, c12, p);		
-                //,%Zd,%Zd\n%Zd,%Zd,%Zd", c11, c12, p, c21, c22, p, c31, c32, p);
-    
-    fclose(fp);
+	fp = fopen("./output", "w+");
+	if (fp == NULL){
+		printf("\"./output\" does not exist.\n");
+		return 0;
+	}
+	gmp_fprintf(fp,"%Zd,%Zd,%Zd\n", c11, c12, p);		
+		//,%Zd,%Zd\n%Zd,%Zd,%Zd", c11, c12, p, c21, c22, p, c31, c32, p);
 
-    return 0;
+	fclose(fp);
+
+	return 0;
 }
